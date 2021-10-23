92ccbeb047653daf1cc24853f70d494f810ee001
Piraka Li <xianglol@umich.edu>
2021-10-23 18:32:07 -0400

    transcribe

--- Transcription/stt.py
+++ Transcription/stt.py
@@ -3,12 +3,8 @@ from google.cloud import speech
 
 def transcribe():
     """Asynchronously transcribes the audio file specified by the gcs_uri."""
-    speech_file = "Videos/Example.mp4"
 
-    with open(speech_file, "rb") as audio_file:
-        content = audio_file.read()
-
-    audio = speech.RecognitionAudio(uri='gcs://sirious_audio/Example.mp4')
+    audio = speech.RecognitionAudio(uri='gs://sirious_audio/Example.flac')
 
     diarization_config = speech.SpeakerDiarizationConfig(
         enable_speaker_diarization=True,
@@ -21,26 +17,44 @@ def transcribe():
     # audio = speech.RecognitionAudio(uri=gcs_uri)
     config = speech.RecognitionConfig(
         encoding=speech.RecognitionConfig.AudioEncoding.FLAC,
-        sample_rate_hertz=16000,
         language_code="en-US",
+        audio_channel_count=2,
         diarization_config=diarization_config,
     )
-
+    print("Recognizing...")
     operation = client.long_running_recognize(config=config, audio=audio)
 
     print("Waiting for operation to complete...")
-    response = operation.result(timeout=90)
+    response = operation.result(timeout=100000)
 
-    out_dict = []
+    out_dict = {}
+
+    # Get full transcription
+    trans_dict = []
     # Each result is for a consecutive portion of the audio. Iterate through
     # them to get the transcripts for the entire audio file.
     for result in response.results:
         # The first alternative is the most likely one for this portion.
-        out_dict.append({})
-        out_dict[-1]["Transcript"] = result.alternatives[0].transcript
-        out_dict[-1]["Confidence"] = result.alternatives[0].confidence
-    
-    with open("output.json", "w") as outfile:
+        trans_dict.append({
+            "transcript": result.alternatives[0].transcript,
+            "confidence": result.alternatives[0].confidence,
+        })
+
+    out_dict['transcription'] = trans_dict
+
+    # Get speaker info
+    speaker_dict = []
+    result = response.results[-1]
+    words_info = result.alternatives[0].words
+    for word_info in words_info:
+        speaker_dict.append({
+            "word": word_info.word,
+            "speaker_tag": word_info.speaker_tag,
+        })
+
+    out_dict["speakers"] = speaker_dict
+
+    with open("transcripts/output.json", "w") as outfile:
         json.dump(out_dict, outfile)
 
 def main():
