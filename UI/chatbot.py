f4652ce83a72ca38d58efd2586edd930ecea22b5
Piraka Li <xianglol@umich.edu>
2021-11-23 19:34:55 -0500

    UI

--- UI/chatbot.py
+++ UI/chatbot.py
@@ -5,10 +5,9 @@ from widgets import *
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
-        self.resize(600, 800)
+        # self.resize(600, 800)
         self.chat_widget = ChatWidget()
         self.lecture_dropdown = LectureDropdown()
-        self.lecture_dropdown.setFocus()
         self.chat_input = ChatInput(self.chat_widget, self.lecture_dropdown)
         self.microphone_button = AudioButton(self.chat_widget, self.lecture_dropdown)
         central_widget = QWidget()
@@ -16,13 +15,21 @@ class MainWindow(QtWidgets.QMainWindow):
         input_hbox = QHBoxLayout()
         input_hbox.addWidget(self.chat_input)
         input_hbox.addWidget(self.microphone_button)
+
         vbox.addWidget(self.lecture_dropdown)
         vbox.addWidget(self.chat_widget)
         vbox.addLayout(input_hbox)
         
         central_widget.setLayout(vbox)
+        # central_widget.setFixedSize(vbox.sizeHint())
         self.setCentralWidget(central_widget)
+        self.center()
 
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
 
 if __name__ == "__main__":
     import sys
--- UI/widgets.py
+++ UI/widgets.py
@@ -1,6 +1,8 @@
 import torch
 import time
 import sys
+import json
+import os
 import pyaudio
 import wave
 import speech_recognition as sr
@@ -12,6 +14,10 @@ from PyQt5.QtWidgets import *
 from tempfile import TemporaryDirectory
 from transformers import AutoTokenizer, AutoModelForQuestionAnswering, pipeline
 
+
+# os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'C:\\Users\\Xianglong\\Desktop\\UMich\\Courses\\EECS 498 Conversation AI\\Project Sirious\\Sirious\\UI\\cert\\key.json'
+# os.environ['GCLOUD_PROJECT'] = 'sirious'
+
 DEBUG = True
 LECTURES = []
 for filename in Path('./lectures').glob('*.txt'):
@@ -19,27 +25,52 @@ for filename in Path('./lectures').glob('*.txt'):
         LECTURES.append(f.read())
 
 R1 = sr.Recognizer()
-R1.pause_threshold = 1
-R1.energy_threshold = 50
+R1.pause_threshold = 0.5
+R1.energy_threshold = 300
+# R1.dynamic_energy_threshold = True
 
 R2 = sr.Recognizer()
 R2.pause_threshold = 1
-R2.energy_threshold = 50
+R2.energy_threshold = 300
+# R1.dynamic_energy_threshold = True
+
+GCLOUD_CREDENTIALS = json.dumps(json.load(open('cert/key.json')))
 
 GUI_FONT = QFont("Arial", 12)
 if not DEBUG:
+    DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     QA_MODEL = AutoModelForQuestionAnswering.from_pretrained(
         'deepset/roberta-base-squad2',
     )
+    QA_MODEL.load_state_dict(torch.load('model/QA_checkpoint/pytorch_model.bin'))
+    # QA_MODEL.to(DEVICE)
     QA_MODEL.eval()
     QA_TOKENIZER = AutoTokenizer.from_pretrained('deepset/roberta-base-squad2')
     QA_PIPELINE = pipeline(
         "question-answering", 
-        model=QA_MODEL,                
+        model=QA_MODEL,
         tokenizer=QA_TOKENIZER,
         device=0,
     )
+    SUM_MODEL = 't5-base'
+    SUM_TOKENIZER = AutoTokenizer.from_pretrained(SUM_MODEL, model_max_len=512)
+    # SUM_TOKENIZER = PegasusTokenizer.from_pretrained('google/bigbird-pegasus-large-arxiv')
+    SUM_PIPELINE = pipeline(
+        "summarization",
+        device=0,
+    )
 
+
+"""
+def bigbird_pegasus(text):
+    inputs = SUM_TOKENIZER([text], max_length=4096, return_tensors='pt', truncation=True)
+    # return inputs['input_ids']
+    # print(text[:1000])
+    # summary_ids = model.generate(inputs['input_ids'], num_beams=8, max_length=512, early_stopping=True)
+    summary_ids = SUM_MODEL.generate(inputs['input_ids'], num_beams=8, max_length=512)
+    # return [tokenizer.decode(g, skip_special_tokens=True, clean_up_tokenization_spaces=False) for g in summary_ids]
+    return SUM_TOKENIZER.batch_decode(summary_ids)
+"""
 
 class HotWordThread(QThread):
     hotword_signal = pyqtSignal()
@@ -47,10 +78,10 @@ class HotWordThread(QThread):
     def run(self):
         with sr.Microphone() as source:
             while True:
-                audio = R2.listen(source)
-                R2.adjust_for_ambient_noise(source)
+                audio = R1.listen(source)
+                R1.adjust_for_ambient_noise(source)
                 try:        
-                    speech = R2.recognize_google(audio).lower()
+                    speech = R1.recognize_google_cloud(audio).lower()
                     if 'hey serious' in speech:
                         self.hotword_signal.emit()
                 except Exception as e:
@@ -60,18 +91,14 @@ class HotWordThread(QThread):
 
 class QuestionThread(QThread):
     question_signal = pyqtSignal(str)
-        
+
     def run(self):
-        """
-        for index, name in enumerate(sr.Microphone.list_microphone_names()):
-            print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
-        """
         with sr.Microphone() as source:
             print('Recognizing...')
             audio = R2.listen(source)
             R2.adjust_for_ambient_noise(source)
             try:        
-                question = R2.recognize_google(audio)
+                question = R2.recognize_google_cloud(audio)
                 print(question)
                 self.question_signal.emit(question)
             except Exception as e:
@@ -88,10 +115,17 @@ class AnswerThread(QThread):
 
     def run(self):
         with torch.no_grad():
-            answer = QA_PIPELINE({
-                'context': self.context, 
-                'question': self.question})['answer']
-            self.answer_signal.emit(answer)
+            if "summarize" in self.question.lower():
+                for block in self.context.split('\n\n'):
+                    if len(block) > 0:
+                        for summary in SUM_PIPELINE(block, min_length=5, max_length=20):
+                            self.answer_signal.emit(summary['summary_text'])
+                    break
+            else:
+                answer = QA_PIPELINE({
+                    'context': self.context, 
+                    'question': self.question})['answer']
+                self.answer_signal.emit(answer)
 
 
 class ChatWidget(QListWidget):
@@ -99,7 +133,16 @@ class ChatWidget(QListWidget):
         def __init__(self, text, is_user=True, parent=None):
             super().__init__(parent)
             self.is_user = is_user
-            self.label = QLabel(text)
+            self.label = QLabel()
+            # self.label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+            self.label.setWordWrap(True)
+            font_metric = QFontMetrics(self.label.font())
+            width = font_metric.width(text)
+            if width < 400:
+                self.label.setMinimumWidth(width + 20)
+            else:
+                self.label.setMinimumWidth(400)
+            self.label.setText(text)
             hbox = QHBoxLayout()
             if is_user:
                 self.label.setStyleSheet(" \
@@ -121,23 +164,30 @@ class ChatWidget(QListWidget):
                 hbox.addStretch(1)
             self.setLayout(hbox)
 
-        def setText(self, text):
-            self.label.setText(text)
-
+        def getText(self):
+            return self.label.text()
 
     def __init__(self, parent=None):
         super(ChatWidget, self).__init__(parent)
+        self.setMinimumSize(600, 800)
         self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.threads = []
     
     def add_message(self, text, is_user=True):
-        chat_item = self.ChatWidgetItem(text, is_user)
+        chat_item = self.ChatWidgetItem(text, is_user, self)
         item = QListWidgetItem()
         item.setSizeHint(chat_item.sizeHint())
         self.addItem(item)
         self.setItemWidget(item, chat_item)
         self.scrollToItem(item)
     
+    def set_message(self, text, item, is_user=True):
+        chat_item = self.ChatWidgetItem(text, is_user, self)
+        item.setSizeHint(chat_item.sizeHint())
+        self.addItem(item)
+        self.setItemWidget(item, chat_item)
+        self.scrollToItem(item)
+
     def answer(self, question, context):
         self.add_message('...', is_user=False)
         if not DEBUG:
@@ -153,7 +203,10 @@ class ChatWidget(QListWidget):
         """
     
     def on_answer_received(self, answer):
-        self.itemWidget(self.item(self.count()-1)).setText(answer)
+        if self.itemWidget(self.item(self.count()-1)).getText() == '...':
+            self.set_message(answer, self.item(self.count()-1), is_user=False)
+        else:
+            self.add_message(answer, is_user=False)
 
 
 class LectureDropdown(QComboBox):
@@ -208,7 +261,7 @@ class AudioButton(QPushButton):
         self.setEnabled(False)
 
     def on_question_received(self, question):
-        self.chat_widget.itemWidget(self.chat_widget.item(self.chat_widget.count()-1)).setText(question)
+        self.chat_widget.set_message(question, self.chat_widget.item(self.chat_widget.count()-1), is_user=True)
         self.setEnabled(True)
         if question == 'Not recognized':
             return
